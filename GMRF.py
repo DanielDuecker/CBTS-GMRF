47e916d445e15a69bea98722e7c1f09094422ded
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-04-02 11:12:18 +0200

    Ground truth added

--- GMRF.py
+++ GMRF.py
@@ -1,32 +1,45 @@
 # First 2D-GMRF Implementation
 
 import numpy as np
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
+from scipy import interpolate
 import math
 import time
 
 # Parameters
 nIter = 1000
 
-# Set up axis
-x = y = np.arange(-2*math.pi,2*math.pi,0.01)
+# Set up axes
+x = y = np.arange(-4,4,0.01)
 X, Y = np.meshgrid(x,y)
 
 # Ground truth
-f = lambda x,y: abs(np.sin(0.9*x)*np.sin(0.9*y))
+xGT = np.array([-4,-2,0,2,4])    # column coordinates
+yGT =  np.array([-4,-2,0,2,4])    # row coordinates
+zGT = np.array([[1,2,2,1,1],
+                [2,4,2,1,1],
+                [1,2,3,3,2],
+                [1,1,2,3,3],
+                [1,1,2,3,3]])
+
+f = interpolate.interp2d(xGT,yGT,zGT)
 
 # Plotting ground truth
-pl.ion()
-fig = pl.figure()
+plt.ion()
+fig = plt.figure()
 
 ax1 = fig.add_subplot(121)
-ax1.contourf(X,Y,f(X,Y))
+ax1.contourf(x,y,f(x,y))
+
+# Show ground thruth interpolation points
+XGT,YGT = np.meshgrid(xGT,yGT)
+ax1.plot(XGT,YGT,marker='.',color='k',linestyle='none')
 
 g = lambda x,y: abs(np.sin(0.9*x)*np.sin(0.9*y))
 ax2 = fig.add_subplot(122)
 ax2.contourf(X,Y,g(X,Y))
 
-pl.show()
+plt.show()
 
 #Update and plot field belief
 for i in range(nIter):
@@ -34,4 +47,3 @@ for i in range(nIter):
     ax2.contourf(X,Y,g(X,Y))
     fig.canvas.draw()
     fig.canvas.flush_events()
-
