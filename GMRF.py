7782a68b9c3b574e887912657b54833243ad4a86
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-04-02 09:24:12 +0200

    Changed to 2D, set up contour plot

--- GMRF.py
+++ GMRF.py
@@ -1,17 +1,17 @@
-# First 1D-GMRF Implementation
+# First 2D-GMRF Implementation
 
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as pl
+import math
+
+x = y = np.arange(-2*math.pi,2*math.pi,0.01)
+X, Y = np.meshgrid(x,y)
 
 # Ground truth
-f = lambda x: abs(np.sin(0.9*x).flatten())
-x = np.linspace(-10,0,100)
-
-# Modeling unobserved locations X as X ~ N( E(X|Y) , )
+f = lambda x,y: abs(np.sin(0.9*x)*np.sin(0.9*y))
 
 # Plotting
 fig = pl.figure()
 pl.clf()
-pl.plot(f(x),x)
+pl.contourf(X,Y,f(X,Y))
 pl.show()
