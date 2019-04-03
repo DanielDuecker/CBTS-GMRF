2b2f27ccf00762648f6ba7701626cc75c5244879
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-04-03 14:33:01 +0200

    Added variance plot

--- GMRF.py
+++ GMRF.py
@@ -9,7 +9,7 @@ import methods
 
 # Parameters
 nIter = 10000
-oz2 = 0.1           # measurement variance
+oz2 = 0.01           # measurement variance
 
 # Set up axes
 x = y = np.arange(-4,4,0.01)
@@ -29,17 +29,26 @@ f = interpolate.interp2d(xGT,yGT,zGT)
 # Plotting ground truth
 plt.ion()
 fig = plt.figure()
-ax1 = fig.add_subplot(121)
+ax1 = fig.add_subplot(221)
 ax1.contourf(x,y,f(x,y))
+plt.title("True field")
 
+##_____GMRF______##
 # Initial GMRF
-nY,nX = zGT.shape                   #CAN BE CHANGED
+nY,nX = zGT.shape
 mu = np.array([np.zeros((nY,nX)).flatten()]).T
-Q = 2*np.random.rand(nY*nX,nY*nX)                   #TO DO: Edit Q Matrix
+QCond = np.eye(nY*nX,nY*nX)
+Q = np.eye(nY*nX,nY*nX)+np.random.rand(nY*nX,nY*nX)                   #TO DO: Edit Q Matrix
+#Q = np.eye(nY*nX)
 
-# Plotting initial belief
-ax2 = fig.add_subplot(122)
+# Plotting initial belief (mean and variance)
+ax2 = fig.add_subplot(222)
 ax2.contourf(xGT,yGT,mu.reshape(nY,nX))
+plt.title("Mean of belief")
+
+ax3 = fig.add_subplot(223)
+ax3.contourf(xGT,yGT,np.diag(QCond).reshape(nY,nX))
+plt.title("Variance of belief")
 
 plt.show()
 
@@ -54,13 +63,13 @@ A = np.array([np.eye(nY*nX)[iPos,:]])
 for i in range(nIter):
     print("Iteration ",i," of ",nIter,".")
 
-    # Update on sigma
-    QCond = (Q+1/oz2*np.dot(A.T,A))
-
-    # Update on mu
+    # Update mean
     temp1 = zMeas - np.dot(A,mu)
     temp2 = np.dot(A.T,temp1)
     mu = mu + 1/oz2*np.dot(np.linalg.inv(QCond),temp2)
+
+    # Update precision matrix
+    QCond = (Q+1/oz2*np.dot(A.T,A))
 
     # Get next measurement at random position, stack under measurement vector
     xMeas = np.random.choice(xGT)
@@ -73,6 +82,7 @@ for i in range(nIter):
 
     # Plotting:
     ax2.contourf(xGT,yGT,mu.reshape(nY,nX))
+    ax3.contourf(xGT,yGT,np.diag(QCond).reshape(nY,nX))
     fig.canvas.draw()
     fig.canvas.flush_events()
     print("New mean: \n",mu)
