6239bcd18f5b5295f943883fe1aa3d3584e992de
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-04-04 11:50:37 +0200

    added timestamps, switched of plotting (takes the most time)
    TO DO:
    - reduce time of sequential plotting
    - augment precision matrix
    - implement random walk

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import methods
 from parameters import gmrf 
 
 # Parameters
-nIter = 10000
+nIter = 100000
 oz2 = 0.01              # measurement variance
 dX = dY = 0.01          #discretizaton in x and y
 
@@ -43,15 +43,26 @@ plt.title("True field")
 #Q = np.eye(gmrf1.nP,gmrf1.nP)+np.random.rand(gmrf1.nP,gmrf1.nP)                   #TO DO: Edit Q Matrix
 Q = methods.getPrecisionMatrix(gmrf1)
 
-# Plotting initial belief (mean and variance)
+# Plotting initial belief (mean,variance and calculation time)
 ax2 = fig.add_subplot(222)
 ax2.contourf(gmrf1.x,gmrf1.y,gmrf1.mu.reshape(gmrf1.nX,gmrf1.nY))
+plt.xlabel("x in m")
+plt.ylabel("y in m")
 plt.title("Mean of belief")
 
 ax3 = fig.add_subplot(223)
 ax3.contourf(gmrf1.x,gmrf1.y,np.diag(gmrf1.prec).reshape(gmrf1.nX,gmrf1.nY))
-plt.title("Variance of belief")
+plt.xlabel("x in m")
+plt.ylabel("y in m")
+plt.title("Precision of belief")
 
+timeVec = []
+iterVec = []
+ax4 = fig.add_subplot(224)
+ax4.plot(iterVec,timeVec)
+plt.xlabel("Iteration index")
+plt.ylabel("calculation time in s")
+plt.title("Update calculation time over iteration index")
 plt.show()
 
 # Get first measurement:
@@ -61,9 +72,10 @@ zMeas = np.array([methods.getMeasurement(xMeas,yMeas,f,oz2)])
 
 A = methods.mapConDis(gmrf1,xMeas,yMeas,zMeas[-1])
 
-#Update and plot field belief
+# Update and plot field belief
 for i in range(nIter):
     print("Iteration ",i," of ",nIter,".")
+    timeBefore = time.time()
 
     # Update mean
     temp1 = zMeas - np.dot(A,gmrf1.mu)
@@ -81,8 +93,23 @@ for i in range(nIter):
     # Map measurement to lattice and stack under A matrix
     A = np.vstack((A,methods.mapConDis(gmrf1,xMeas,yMeas,zMeas[-1])))
 
+    timeAfter = time.time()
+    iterVec.append(i)
+    timeVec.append(timeAfter-timeBefore)
+    print("timeAfter-timeBefore:",timeAfter-timeBefore)
+
     # Plotting:
-    ax2.contourf(gmrf1.x,gmrf1.y,gmrf1.mu.reshape(gmrf1.nX,gmrf1.nY))
-    ax3.contourf(gmrf1.x,gmrf1.y,np.diag(gmrf1.prec).reshape(gmrf1.nX,gmrf1.nY))
-    fig.canvas.draw()
-    fig.canvas.flush_events()
+    #ax2.contourf(gmrf1.x,gmrf1.y,gmrf1.mu.reshape(gmrf1.nX,gmrf1.nY))
+    #ax3.contourf(gmrf1.x,gmrf1.y,np.diag(gmrf1.prec).reshape(gmrf1.nX,gmrf1.nY))
+    #ax4.plot(iterVec,timeVec,'black')
+    #fig.canvas.draw()
+    #fig.canvas.flush_events()
+
+ax2.contourf(gmrf1.x,gmrf1.y,gmrf1.mu.reshape(gmrf1.nX,gmrf1.nY))
+ax3.contourf(gmrf1.x,gmrf1.y,np.diag(gmrf1.prec).reshape(gmrf1.nX,gmrf1.nY))
+ax4.plot(iterVec,timeVec,'black')
+fig.canvas.draw()
+fig.canvas.flush_events()
+
+while True:
+    wait = True
