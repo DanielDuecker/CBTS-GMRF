c21f44a5ebff4c7e174198bbbc95fdf74c611680
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-04-23 16:22:58 +0200

    F,G,H are now matrices

--- STKF.py
+++ STKF.py
@@ -2,6 +2,7 @@ import classes
 import methods
 import math
 import scipy
+import numpy as np
 
 xMin = 0
 xMax = 10
@@ -9,16 +10,17 @@ nX = 10
 yMin = 0
 yMax = 10
 nY = 10
+nBeta = 0
 
-gmrf1 = classes.gmrf(xMin,xMax,nX,yMin,yMax,nY)
+gmrf1 = classes.gmrf(xMin, xMax, nX, yMin, yMax, nY, nBeta)
 
 sigmaT = 0.01
 lambd = 1
 
 # State representation of Sr
-F = -1/sigmaT
-H = math.sqrt(2*lambd / sigmaT)
-G = 1
+F = -1/sigmaT * np.ones((1,1))
+H = math.sqrt(2*lambd / sigmaT) * np.ones((1,1))
+G = np.ones((1,1))
 simga2 = 0.01
 
 # Kernels
