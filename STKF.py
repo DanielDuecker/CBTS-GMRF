835ed07347e2fc83553947dbddefe7dc6062163c
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-04-24 15:46:37 +0200

    First untested implementation of discrete matrices, Kalman Rgression still missing

--- STKF.py
+++ STKF.py
@@ -6,7 +6,7 @@ import numpy as np
 
 T = 100
 dt = 0.1
-nIter = T/dt
+nIter = int(T/dt)
 
 xMin = 0
 xMax = 10
@@ -21,6 +21,10 @@ gmrf1 = classes.gmrf(xMin, xMax, nX, yMin, yMax, nY, nBeta)
 sigmaT = 0.01
 lambd = 1
 
+xHist = [par.x0]  # x-state history vector
+yHist = [par.y0]  # y-state history vector
+
+
 # State representation of Sr
 F = -1/sigmaT * np.ones((1,1))
 H = math.sqrt(2*lambd / sigmaT) * np.ones((1,1))
@@ -29,27 +33,39 @@ simga2 = 0.01
 
 # Kernels
 Ks = methods.getPrecisionMatrix(gmrf1)
+KsChol = np.linalg.cholesky(Ks)
 h = lambda tau: lambd * math.exp(-abs(tau)/sigmaT)
 
 sigmaZero = scipy.linalg.solve_lyapunov(F, G*G.T)
 
 # Initialization
-sHeadZero = 0
-sigma = np.kron(np.eye(sigmaZero.shape[0]), sigmaT)
+sZero = 0
+Cov = np.kron(np.eye(sigmaZero.shape[0]), sigmaT)
 
 tk = 0
 
 for i in range(nIter):
     t = i*dt
-    if t-tk =! 0:
-        sHead = math.exp(np.kron(eye(F.shape[0]),F)*(t-tk))*skk
-        sigmaS = 666
+    A = scipy.linalg.expm(np.kron(np.eye(F.shape[0]), F) * (t - tk))
+
+    (xMeas, yMeas) = methods.getNextState(xMeas, yMeas, xHist[-2], yHist[-2], par.maxStepsize, gmrf1)
+    xHist.append(xMeas)
+    yHist.append(yMeas)
+    zMeas[(i+1) % par.nMeas] = methods.getMeasurement(xMeas, yMeas, trueField, par.ov2)
+
+    if (t-tk)!=0:
+        # Open loop prediciton
+        sHead = A*skk
+        sigmaS = np.dot(A,np.dot(Covkk,A.T))
     else:
-        A = 666
-        C = 666
-        Q = 666
-        R = 666
+        Phi = methods.mapConDis(gmrf1, xMeas, yMeas)
+        C = np.dot(Phi,np.dot(KsChol,np.kron(eye(H.shape[0]), H)))
+        QBar = scipy.integrate.quad(lambda tau: np.dot(scipy.linalg.expmnp.dot(F,tau),np.dot(G,np.dot(G.T,scipy.linalg.expmnp.dot(F,tau.T)))),0,dt)
+        Q = np.kron(np.eye(QBar.shape[0]), QBar)
+        R =sigma2
+
         #Kalman Regression
+
         tk = t
     f = 666
     sigmaF = 666
