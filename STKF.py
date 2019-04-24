a8df918f6c0e26d457d25aa4ec18e9a9b826b082
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-04-24 16:26:59 +0200

    Dicrete matrices are implemented properly

--- STKF.py
+++ STKF.py
@@ -3,6 +3,7 @@ import methods
 import parameters as par
 import math
 import scipy
+from scipy import  integrate
 import numpy as np
 
 T = 100
@@ -38,7 +39,7 @@ yMeas = par.y0
 F = -1/sigmaT * np.ones((1,1))
 H = math.sqrt(2*lambd / sigmaT) * np.ones((1,1))
 G = np.ones((1,1))
-simga2 = 0.01
+sigma2 = 0.01
 
 # Kernels
 Ks = methods.getPrecisionMatrix(gmrf1)
@@ -49,13 +50,15 @@ sigmaZero = scipy.linalg.solve_lyapunov(F, G*G.T)
 
 # Initialization
 sZero = 0
-Cov = np.kron(np.eye(sigmaZero.shape[0]), sigmaT)
+Cov = np.kron(np.eye(gmrf1.nP), sigmaT)
 
 tk = 0
 
+skk = 1 #Fix this
+Covkk = 1#Fix this
 for i in range(nIter):
     t = i*dt
-    A = scipy.linalg.expm(np.kron(np.eye(F.shape[0]), F) * (t - tk))
+    A = scipy.linalg.expm(np.kron(np.eye(gmrf1.nP), F) * (t - tk))
 
     zMeas = methods.getMeasurement(xMeas, yMeas, trueField, par.ov2)
     (xMeas, yMeas) = methods.getNextState(xMeas, yMeas, xHist[-1], yHist[-1], par.maxStepsize, gmrf1)
@@ -68,16 +71,15 @@ for i in range(nIter):
         sigmaS = np.dot(A,np.dot(Covkk,A.T))
     else:
         Phi = methods.mapConDis(gmrf1, xMeas, yMeas)
-        print(KsChol.shape)
-        print(np.kron(np.eye(H.shape[0]), H).shape)
-        C = np.dot(Phi,np.dot(KsChol,np.kron(np.eye(H.shape[0]), H)))
-        QBar = scipy.integrate.quad(lambda tau: np.dot(scipy.linalg.expmnp.dot(F,tau),np.dot(G,np.dot(G.T,scipy.linalg.expmnp.dot(F,tau.T)))),0,dt)
-        Q = np.kron(np.eye(QBar.shape[0]), QBar)
-        R =sigma2
+        C = np.dot(Phi,np.dot(KsChol,np.kron(np.eye(gmrf1.nP), H)))
+        QBar = scipy.integrate.quad(lambda tau: np.dot(scipy.linalg.expm(np.dot(F,tau)),np.dot(G,np.dot(G.T,scipy.linalg.expm(np.dot(F,tau).T)))),0,dt)
+        Q = np.kron(np.eye(gmrf1.nP), QBar)
+        R = sigma2
 
         #Kalman Regression
-
+        # Add regression here
         tk = t
+
     f = 666
     sigmaF = 666
 
