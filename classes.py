Tidied up. Deleted single STKF file. STKF covariance calculation seems to have a bug (unvisited states have low covariance)

classes.py
@@ -7,8 +7,9 @@ import math
 import parameters as par
 
 from scipy import interpolate
-from scipy import  integrate
+from scipy import integrate
 import scipy
+
 
 class gmrf:
     def __init__(self, xMin, xMax, nX, yMin, yMax, nY, nBeta):
@@ -45,7 +46,7 @@ class gmrf:
         covPriorLowerRight = self.Tinv
         self.covPrior = np.vstack(
             (np.hstack((covPriorUpperLeft, covPriorUpperRight)), np.hstack((covPriorLowerLeft, covPriorLowerRight))))
-        self.meanPrior = np.zeros((self.nP+self.nBeta, 1))
+        self.meanPrior = np.zeros((self.nP + self.nBeta, 1))
 
         # Initialize augmented conditioned mean, covariance and precision matrices
         self.meanCond = np.zeros((self.nP + self.nBeta, 1))
@@ -64,8 +65,8 @@ class gmrf:
         temp2 = np.dot(np.linalg.inv(R), temp1)
         temp3 = np.dot(Phi.T, temp2)
 
-        self.covCond1 = self.covPrior - np.dot(self.covPrior, temp3)
-        #self.covCond2 = np.linalg.inv((np.linalg.inv(self.covPrior)+1/par.ov2*np.dot(Phi.T,Phi))) # alternative way
+        self.covCond = self.covPrior - np.dot(self.covPrior, temp3)
+        # self.covCond = np.linalg.inv((np.linalg.inv(self.covPrior)+1/par.ov2*np.dot(Phi.T,Phi))) # alternative way
         self.diagCovCond = self.covCond.diagonal().reshape(self.nP + self.nBeta, 1)
 
         "Update mean"
@@ -81,13 +82,14 @@ class gmrf:
 
         self.bSeq = self.bSeq + 1 / par.ov2 * Phi_k.T * zMeas_k  # sequential update canonical mean
         self.precCond = self.precCond + 1 / par.ov2 * np.dot(Phi_k.T, Phi_k)  # sequential update of precision matrix
-        self.diagCovCond = np.linalg.inv(self.precCond).diagonal().reshape(self.nP+self.nBeta, 1)  # works too
+        self.diagCovCond = np.linalg.inv(self.precCond).diagonal().reshape(self.nP + self.nBeta, 1)  # works too
         self.meanCond = np.dot(np.linalg.inv(self.precCond), self.bSeq)
 
         # TODO: Fix calculation of covariance diagonal
-        #hSeq = np.linalg.solve(self.precCond, Phi_k.T)
-        #self.diagCovCond = self.diagCovCond - 1 / (par.ov2 + np.dot(Phi_k, hSeq)[0, 0]) * np.dot(hSeq,
+        # hSeq = np.linalg.solve(self.precCond, Phi_k.T)
+        # self.diagCovCond = self.diagCovCond - 1 / (par.ov2 + np.dot(Phi_k, hSeq)[0, 0]) * np.dot(hSeq,
         #                                                            hSeq.T).diagonal().reshape(self.nP + self.nBeta, 1)
+
 
 class trueField:
     def __init__(self, xEnd, yEnd, sinusoidal, temporal):
@@ -104,42 +106,43 @@ class trueField:
         if self.sinusoidal:
             xGT = np.arange(par.xMin, par.xMax, par.dX)
             yGT = np.arange(par.yMin, par.yMax, par.dY)
-            XGT, YGT = np.meshgrid(xGT,yGT)
-            zGT = np.sin(XGT)+np.sin(YGT)
+            XGT, YGT = np.meshgrid(xGT, yGT)
+            zGT = np.sin(XGT) + np.sin(YGT)
             self.fInit = interpolate.interp2d(xGT, yGT, zGT)
         else:
             xGT = np.array([0, 2, 4, 6, 9])  # column coordinates
             yGT = np.array([0, 1, 3, 5, 9])  # row coordinates
-            #zGT = np.array([[1, 2, 2, 1, 1],
-                            #[2, 4, 2, 1, 1],
-                            #[1, 2, 3, 3, 2],
-                            #[1, 1, 2, 3, 3],
-                            #[1, 1, 2, 3, 3]])
+            # zGT = np.array([[1, 2, 2, 1, 1],
+            # [2, 4, 2, 1, 1],
+            # [1, 2, 3, 3, 2],
+            # [1, 1, 2, 3, 3],
+            # [1, 1, 2, 3, 3]])
 
             zGT = np.array([[2, 4, 6, 7, 8],
-                          [2.1, 5, 7, 11.25, 9.5],
-                          [3, 5.6, 8.5, 17, 14.5],
-                          [2.5, 5.4, 6.9, 9, 8],
-                          [2, 2.3, 4, 6, 7.5]])
+                            [2.1, 5, 7, 11.25, 9.5],
+                            [3, 5.6, 8.5, 17, 14.5],
+                            [2.5, 5.4, 6.9, 9, 8],
+                            [2, 2.3, 4, 6, 7.5]])
 
         self.fInit = interpolate.interp2d(xGT, yGT, zGT)
 
     def field(self, x, y):
         if not par.sinusoidal:
-            return self.fInit(x-self.xShift, y+self.yShift)
+            return self.fInit(x - self.xShift, y + self.yShift)
         else:
-            return self.cScale*self.fInit(x, y)
+            return self.cScale * self.fInit(x, y)
 
     def updateField(self, t):
-        self.xShift = par.dxdt*t % self.xEnd
-        self.yShift = par.dydt*t % self.yEnd
+        self.xShift = par.dxdt * t % self.xEnd
+        self.yShift = par.dydt * t % self.yEnd
         if t < par.pulseTime:
-            self.cScale = np.cos(math.pi*t/par.pulseTime)
+            self.cScale = np.cos(math.pi * t / par.pulseTime)
+
 
 class stkf:
-    def __init__(self,xMin, xMax, nX, yMin, yMax, nY, nBeta,trueField,dt,sigmaT,lambd,sigma2):
+    def __init__(self, xMin, xMax, nX, yMin, yMax, nY, nBeta, trueF, dt, sigmaT, lambd, sigma2):
         self.gmrf = gmrf(xMin, xMax, nX, yMin, yMax, nY, nBeta)
-        self.trueField = trueField
+        self.trueField = trueF
         self.dt = dt
         self.sigmaT = sigmaT
         self.lambd = lambd
@@ -152,7 +155,7 @@ class stkf:
 
         # Kernels
         self.Ks = methods.getPrecisionMatrix(self.gmrf)
-        self.KsChol = scipy.linalg.cholesky(self.Ks)
+        self.KsChol = np.linalg.cholesky(self.Ks)
         # h = lambda tau: lambd * math.exp(-abs(tau) / sigmaT)
 
         self.sigmaZero = scipy.linalg.solve_lyapunov(self.F, self.G * self.G.T)
@@ -162,34 +165,35 @@ class stkf:
         self.covkk = np.kron(np.eye(self.gmrf.nP), self.sigmaZero)
         self.tk = 0
 
-    def kalmanFilter(self,t,xMeas,yMeas,zMeas):
-            A = scipy.linalg.expm(np.kron(np.eye(self.gmrf.nP), self.F) * (t - self.tk))
-            if t % 1 != 0:
-                # Open loop prediciton
-                st = np.dot(A, self.skk)
-                covt = np.dot(A, np.dot(self.covkk, A.T))
-            else:
-                Phi = methods.mapConDis(self.gmrf, xMeas, yMeas)
-                C = np.dot(Phi, np.dot(self.KsChol, np.kron(np.eye(self.gmrf.nP), self.H)))
-                QBar = scipy.integrate.quad(lambda tau: np.dot(scipy.linalg.expm(np.dot(self.F, tau)), np.dot(self.G, np.dot(self.G.T, scipy.linalg.expm(np.dot(self.F,tau).T)))),0, self.dt)[0]
-                Q = np.kron(np.eye(self.gmrf.nP), QBar)
-                R = self.sigma2
+    def kalmanFilter(self, t, xMeas, yMeas, zMeas):
+        A = scipy.linalg.expm(np.kron(np.eye(self.gmrf.nP), self.F) * (t - self.tk))
+        if t % 1 != 0:
+            # Open loop prediciton
+            st = np.dot(A, self.skk)
+            covt = np.dot(A, np.dot(self.covkk, A.T))
+        else:
+            Phi = methods.mapConDis(self.gmrf, xMeas, yMeas)
+            C = np.dot(Phi, np.dot(self.KsChol, np.kron(np.eye(self.gmrf.nP), self.H)))
+            QBar = scipy.integrate.quad(lambda tau: np.dot(scipy.linalg.expm(np.dot(self.F, tau)), np.dot(self.G,
+                                            np.dot(self.G.T, scipy.linalg.expm(np.dot(self.F, tau).T)))), 0, self.dt)[0]
+            Q = np.kron(np.eye(self.gmrf.nP), QBar)
+            R = self.sigma2
 
-                # Kalman Regression
-                sPred = np.dot(A, self.skk)
-                covPred = np.dot(A, np.dot(self.covkk, A.T)) + Q
+            # Kalman Regression
+            sPred = np.dot(A, self.skk)
+            covPred = np.dot(A, np.dot(self.covkk, A.T)) + Q
 
-                kalmanGain = np.dot(covPred, np.dot(C.T, np.linalg.inv(np.dot(C, np.dot(covPred, C.T)) + R)))
-                sUpdated = sPred + np.dot(kalmanGain, zMeas - np.dot(C, sPred))
-                covUpdated = np.dot(np.eye(self.gmrf.nP) - np.dot(kalmanGain, C), covPred)
-                self.skk = sUpdated
-                self.covkk = covUpdated
+            kalmanGain = np.dot(covPred, np.dot(C.T, np.linalg.inv(np.dot(C, np.dot(covPred, C.T)) + R)))
+            sUpdated = sPred + np.dot(kalmanGain, zMeas - np.dot(C, sPred))
+            covUpdated = np.dot(np.eye(self.gmrf.nP) - np.dot(kalmanGain, C), covPred)
+            self.skk = sUpdated
+            self.covkk = covUpdated
 
-                st = sUpdated
-                covt = covUpdated
-                self.tk = t
+            st = sUpdated
+            covt = covUpdated
+            self.tk = t
 
-            hAug = np.kron(np.eye(self.gmrf.nP), self.H)
-            self.gmrf.meanCond = np.dot(self.KsChol, np.dot(hAug, st))
-            self.gmrf.covCond = np.dot(self.KsChol, np.dot(hAug, np.dot(covt, np.dot(hAug.T, self.KsChol))))
-            self.gmrf.diagCovCond = self.gmrf.covCond.diagonal()
+        hAug = np.kron(np.eye(self.gmrf.nP), self.H)
+        self.gmrf.meanCond = np.dot(self.KsChol, np.dot(hAug, st))
+        self.gmrf.covCond = np.dot(self.KsChol, np.dot(hAug, np.dot(covt, np.dot(hAug.T, self.KsChol))))
+        self.gmrf.diagCovCond = self.gmrf.covCond.diagonal()

main.py
@@ -8,7 +8,7 @@ import numpy as np
 import methods
 import parameters as par
 from classes import gmrf
-from classes import  stkf
+from classes import stkf
 from classes import trueField
 
 np.set_printoptions(threshold=np.inf)
@@ -32,7 +32,8 @@ X, Y = np.meshgrid(x, y)
 trueField = trueField(x[-1], y[-1], par.sinusoidal, par.temporal)
 
 """STKF"""
-stkf1 = stkf(par.xMin, par.xMax, par.nX, par.yMin, par.yMax, par.nY, par.nBeta,trueField,par.dt,par.sigmaT,par.lambd,par.sigma2)
+stkf1 = stkf(par.xMin, par.xMax, par.nX, par.yMin, par.yMax, par.nY, par.nBeta, trueField, par.dt, par.sigmaT,
+             par.lambd, par.sigma2)
 
 """GMRF"""
 # Initialize Plot
@@ -46,15 +47,15 @@ xHist.append(xMeas)
 yHist.append(yMeas)
 
 # Initialize measurement vector and mapping matrix
-zMeas = np.zeros((par.nMeas,1))
-Phi = np.zeros((par.nMeas,gmrf1.nP+gmrf1.nBeta))
+zMeas = np.zeros((par.nMeas, 1))
+Phi = np.zeros((par.nMeas, gmrf1.nP + gmrf1.nBeta))
 zMeas[0] = methods.getMeasurement(xMeas, yMeas, trueField, par.ov2)
 Phi[0, :] = methods.mapConDis(gmrf1, xMeas, yMeas)
 
 # Update and plot field belief
-for i in range(par.nIter-1):
+for i in range(par.nIter - 1):
     print("Iteration ", i, " of ", par.nIter, ".")
-    t = i*par.dt
+    t = i * par.dt
 
     timeBefore = time.time()
 
@@ -74,14 +75,14 @@ for i in range(par.nIter-1):
     (xMeas, yMeas) = methods.getNextState(xMeas, yMeas, xHist[-2], yHist[-2], par.maxStepsize, gmrf1)
     xHist.append(xMeas)
     yHist.append(yMeas)
-    zMeas[(i+1) % par.nMeas] = methods.getMeasurement(xMeas, yMeas, trueField, par.ov2)
+    zMeas[(i + 1) % par.nMeas] = methods.getMeasurement(xMeas, yMeas, trueField, par.ov2)
 
     # Map measurement to surrounding grid vertices and stack under Phi matrix
-    Phi[(i+1) % par.nMeas, :] = methods.mapConDis(gmrf1, xMeas, yMeas)
+    Phi[(i + 1) % par.nMeas, :] = methods.mapConDis(gmrf1, xMeas, yMeas)
 
     # If truncated measurements are used, set conditioned mean and covariance as prior
     if par.truncation:
-        if (i+1) % par.nMeas == 0:
+        if (i + 1) % par.nMeas == 0:
             gmrf1.covPrior = gmrf1.covCond
             gmrf1.meanPrior = gmrf1.meanCond
 
@@ -92,7 +93,7 @@ for i in range(par.nIter-1):
 
     # Plotting:
     if not par.fastCalc:
-        #if i % (par.nIter/100) == 0:
+        # if i % (par.nIter/100) == 0:
         methods.plotFields(fig, x, y, trueField, gmrf1, iterVec, timeVec, xHist, yHist)
 
     # Update ground truth:
@@ -104,7 +105,7 @@ plt.show(block=True)
 
 print("Last updates needed approx. ", np.mean(timeVec[-100:-1]), " seconds per iteration.")
 
-#TODO augment to time dependent fields
-#TODO add outer grid
-#TODO boundary conditions
-#TODO use of sparse commands
+# TODO check covariance calculation for STKF
+# TODO add outer grid
+# TODO boundary conditions
+# TODO use of sparse commands

methods.py
@@ -4,6 +4,7 @@ import numpy as np
 
 import parameters as par
 
+
 def getMeasurement(xMeas, yMeas, trueField, noiseVariance):
     noise = np.random.normal(0, math.sqrt(noiseVariance))
     return (trueField.field(xMeas, yMeas) + noise)[0]
@@ -37,35 +38,20 @@ def mapConDis(gmrf, xMeas, yMeas):
     return Phi
 
 
-# def getPrecisionMatrix(gmrf):
-#    diagQ = 2*np.eye(gmrf.nP)
-#    Lambda = diagQ-np.eye(gmrf.nP,k=1)-np.eye(gmrf.nP,k=-1)
-#    return Lambda
-
 def getPrecisionMatrix(gmrf):
-    #diagQ = 2 * np.eye(gmrf.nP)
-    #Lambda = diagQ - np.eye(gmrf.nP, k=1) - np.eye(gmrf.nP, k=-1)
-    Lambda = np.array([[5,-1,0,-1,0,0,0,0,0],
-                    [-1,3,-1,0,-1,0,0,0,0],
-                    [0,-1,2,0,0,-1,0,0,0],
-                    [-1,0,0,3,-1,0,-1,0,0],
-                    [0,-1,0,-1,4,-1,0,-1,0],
-                    [0,0,-1,0,-1,3,0,0,-1],
-                    [0,0,0,-1,0,0,2,-1,0],
-                    [0,0,0,0,-1,0,-1,3,-1],
-                    [0,0,0,0,0,-1,0,-1,2]])
-    Lambda = 4*np.eye(gmrf.nP) - np.eye(gmrf.nP, k=gmrf.nX) - np.eye(gmrf.nP, k=-gmrf.nX)
-    Lambda = Lambda - np.eye(gmrf.nP, k=1) - np.eye(gmrf.nP, k=-1)
+    diagonalValue = 4  # needs to be high enough in order to create a strictly diagonal dominant matrix
+    Lambda = diagonalValue * np.eye(gmrf.nP) - np.eye(gmrf.nP, k=gmrf.nX) - np.eye(gmrf.nP, k=-gmrf.nX)
+    Lambda -= np.eye(gmrf.nP, k=1) - np.eye(gmrf.nP, k=-1)
 
+    # set precision entry to zero if left or right border is reached
+    # (since there is no connection between the two edge vertices)
     for i in range(gmrf.nP):
-        if (i % gmrf.nX) == 0: #left border
+        if (i % gmrf.nX) == 0:  # left border
             if i >= gmrf.nX:
-                Lambda[i,i-1] = 0
-        if (i % gmrf.nX) == (gmrf.nX-1): #right border
-            if i <= (gmrf.nP-gmrf.nX):
-                Lambda[i,i+1] = 0
-    print(Lambda)
-    #print(np.linalg.eig(Lambda))
+                Lambda[i, i - 1] = 0
+        if (i % gmrf.nX) == (gmrf.nX - 1):  # right border
+            if i <= (gmrf.nP - gmrf.nX):
+                Lambda[i, i + 1] = 0
     return Lambda
 
 
@@ -81,12 +67,17 @@ def getNextState(x, y, xBefore, yBefore, maxStepsize, gmrf):
         yNext = np.random.choice([y - stepsize, y + stepsize])
 
     # x and y are switched because of matrix/plot relation
-    (yUncertain,xUncertain) = np.unravel_index(np.argmax(gmrf.diagCovCond[0:gmrf.nP].reshape(gmrf.nY, gmrf.nX),axis=None),(gmrf.nY,gmrf.nX))
+    (yUncertain, xUncertain) = np.unravel_index(
+        np.argmax(gmrf.diagCovCond[0:gmrf.nP].reshape(gmrf.nY, gmrf.nX), axis=None), (gmrf.nY, gmrf.nX))
 
-    dist = np.sqrt((xUncertain-x)**2+(yUncertain-y)**2)
+    dist = np.sqrt((xUncertain - x) ** 2 + (yUncertain - y) ** 2)
+
     if np.random.rand() < par.exploitingRate:
-        xNext = x + par.xVel*(xUncertain-x)/dist
-        yNext = y + par.yVel*(yUncertain-y)/dist
+        xNext = x + par.xVel * (xUncertain - x) / dist
+        yNext = y + par.yVel * (yUncertain - y) / dist
+        if dist == 0:
+            xNext = x
+            yNext = y
 
     if xNext < gmrf.xMin:
         xNext = x + stepsize
@@ -107,7 +98,7 @@ def plotFields(fig, x, y, trueField, gmrf, iterVec, timeVec, xHist, yHist):
 
     # Plotting ground truth
     ax1 = fig.add_subplot(221)
-    ax1.contourf(x, y, trueField.field(x,y))
+    ax1.contourf(x, y, trueField.field(x, y))
     plt.title("True field")
 
     # Plotting conditioned mean
@@ -117,7 +108,7 @@ def plotFields(fig, x, y, trueField, gmrf, iterVec, timeVec, xHist, yHist):
     plt.ylabel("y in m")
     plt.title("Mean of belief")
 
-    # Plotting precision matrix
+    # Plotting covariance matrix
     ax3 = fig.add_subplot(223)
     ax3.contourf(gmrf.x, gmrf.y, gmrf.diagCovCond[0:gmrf.nP].reshape(gmrf.nY, gmrf.nX))
     ax3.plot(xHist, yHist, 'black')