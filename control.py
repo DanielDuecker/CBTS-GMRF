25a763879131f950395b08dc4ab22c6a4717731c
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-05-06 15:45:51 +0200

    Cost of path segments implemented

--- control.py
+++ control.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import parameters as par
+import methods
 
 class piControl:
     def __init__(self,R,g,varNoise,H,K,dt,nUpdated):
@@ -15,13 +16,12 @@ class piControl:
         self.u0 = np.zeros((self.H,1))
         self.u = self.u0
 
-    def getAction(self, x, y):
+    def getAction(self, gmrf, x, y):
         self.u0 = self.u
         M = np.dot(np.linalg.inv(self.R), np.dot(self.g, self.g.T))
 
         for n in range(self.nUpdated):
             for k in range(self.K):
-                fig2 = plt.figure()
                 noise = np.zeros((self.H, 1))
                 pathRollOut = np.zeros((2, self.H))
                 # sample control noise and compute path roll-outs
@@ -30,9 +30,11 @@ class piControl:
                     pathRollOut[0, j] = x + par.xVel*math.cos(self.u0[j]+noise[j])
                     pathRollOut[1, j] = y + par.yVel*math.sin(self.u0[j]+noise[j])
 
-                for i in range(H):
-                    test=1
-                plt.plot(pathRollOut[0, :], pathRollOut[1, :])
-                plt.show(block=True)
-                while(1):
-                    t=1
+                S = np.zeros((1,self.H+1))
+                for i in range(self.H):
+                    index = self.H-i-1
+                    Phi = methods.mapConDis(gmrf, pathRollOut[0, index], pathRollOut[1, index])
+                    qhh = np.dot(Phi, np.dot(gmrf.covCond, Phi.T))
+                    S[0,index] = S[0,index+1] + qhh + 0.5*np.dot((self.u[index,0]+np.dot(M[index,index],noise[index,0])).T,np.dot(self.R[index,index],self.u[index,0]+np.dot(M[index,index],noise[index,0])))
+                print(S)
+
