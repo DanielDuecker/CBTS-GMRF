Fixed bug in Layapunov equation for sigma zero and bug in precision matrix

classes.py
@@ -158,7 +158,7 @@ class stkf:
         self.KsChol = np.linalg.cholesky(self.Ks)
         # h = lambda tau: lambd * math.exp(-abs(tau) / sigmaT)
 
-        self.sigmaZero = scipy.linalg.solve_lyapunov(self.F, self.G * self.G.T)
+        self.sigmaZero = scipy.linalg.solve_continuous_lyapunov(self.F, -self.G * self.G.T)
 
         # Initialization
         self.skk = np.zeros((self.gmrf.nP, 1))

methods.py
@@ -41,7 +41,7 @@ def mapConDis(gmrf, xMeas, yMeas):
 def getPrecisionMatrix(gmrf):
     diagonalValue = 4  # needs to be high enough in order to create a strictly diagonal dominant matrix
     Lambda = diagonalValue * np.eye(gmrf.nP) - np.eye(gmrf.nP, k=gmrf.nX) - np.eye(gmrf.nP, k=-gmrf.nX)
-    Lambda -= np.eye(gmrf.nP, k=1) - np.eye(gmrf.nP, k=-1)
+    Lambda -= np.eye(gmrf.nP, k=1) + np.eye(gmrf.nP, k=-1)
 
     # set precision entry to zero if left or right border is reached
     # (since there is no connection between the two edge vertices)