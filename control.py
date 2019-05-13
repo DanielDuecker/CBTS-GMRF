After fixing the probability calculation (1e-10 was too large as initial sum since the high costs of longer pathes lead to very small probabilities. Agent is bouncing off of walls, but paths don't change direction

control.py
@@ -10,7 +10,7 @@ class piControl:
         self.R = R
         self.g = g
         self.lambd = lambd
-        self.varNoise = self.lambd*np.linalg.inv(R)
+        self.varNoise = self.lambd*np.linalg.inv(self.R)
         self.H = H
         self.K = K
         self.dt = dt
@@ -70,7 +70,7 @@ class piControl:
             P = np.zeros((self.H, self.K))
             for k in range(self.K):
                 for i in range(self.H):
-                    probSum = 1e-10
+                    probSum = 1e-1000
                     for indexSum in range(self.K):
                         probSum += math.exp(-S[i, indexSum]/self.lambd)
                     P[i, k] = math.exp(-S[i, k]/self.lambd)/probSum

parameters.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Parameters for all files
 # main.py
-stkf = True
+stkf = False
 sequentialUpdate = True  # Does not work with truncation!
 fastCalc = False  # True: Fast Calculation, only one plot in the end; False: Live updating and plotting
 truncation = False
@@ -43,14 +43,14 @@ lambd = 1
 sigma2 = 0.01
 
 # PI2 controller
-H = 15 # control horizon steps
+H = 10 # control horizon steps
 controlCost = 1e3 #1e3
 R = controlCost*np.eye(H)
 g = np.ones((H,1))
 lambd = 100 #100
-K = 10
+K = 3
 ctrSamplingTime = 0.01
-nUpdated = 10
+nUpdated = 3
 
 if not truncation:
     nMeas = nIter