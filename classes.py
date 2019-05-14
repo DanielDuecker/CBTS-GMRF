Fixed a bug in path probability calculation. Thursday: Read paper about PI2

classes.py
@@ -200,8 +200,8 @@ class stkf:
 
         #self.gmrf.covCond = 20*np.eye(self.gmrf.nP+self.gmrf.nBeta)
         #for i in range(self.gmrf.nP+self.gmrf.nBeta):
-        #    if i%5 == 0:
+         #   if i%5 == 0:
         #        self.gmrf.covCond[i, i+1] = 10
-        #        self.gmrf.covCond[i, i] = 10
-        #        self.gmrf.covCond[i+1, i] = 10
+       #         self.gmrf.covCond[i, i] = 10
+       #         self.gmrf.covCond[i+1, i] = 10
         #self.gmrf.diagCovCond = self.gmrf.covCond.diagonal()

control.py
@@ -53,19 +53,13 @@ class piControl:
             noise = np.zeros((self.H, self.K))
             self.xPathRollOut = np.zeros((self.H, self.K))
             self.yPathRollOut = np.zeros((self.H, self.K))
-
             S = np.zeros((self.H + 1, self.K))
+
             for k in range(self.K):
                 # sample control noise and compute path roll-outs
                 for j in range(self.H):
                     noise[j, k] = np.random.normal(0, math.sqrt(self.varNoise[j, j]))
                     (xTrVec, yTrVec, alphaNew) = agent.trajectoryFromControl(self.u[:, 0] + noise[:, k])
-
-                    # Todo: remove this
-                    # repeat if states are out of bound
-                    #while not methods.sanityCheck(xTrVec[:, 0], yTrVec[:, 0], gmrf):
-                    #    noise[j, k] = np.random.normal(math.pi/4, 2*math.sqrt(self.varNoise[j, j]))
-                    #    (xTrVec, yTrVec, alphaNew) = agent.trajectoryFromControl(self.u[:, 0] + noise[:, k])
 
                     self.xPathRollOut[:, k] = xTrVec[:, 0]
                     self.yPathRollOut[:, k] = yTrVec[:, 0]
@@ -85,12 +79,14 @@ class piControl:
             # Normalize state costs
             S = S/np.amax(S)
 
-            # Compute probability of path segments
-            P = np.zeros((self.H, self.K))
+            # Compute cost of path segments
             expS = np.zeros((self.H, self.K))
             for k in range(self.K):
                 for i in range(self.H):
                     expS[i,k] = math.exp(-S[i, k]/self.lambd)
+
+            P = np.zeros((self.H, self.K))
+            for k in range(self.K):
                 for i in range(self.H):
                     P[i, k] = expS[i, k] / sum(expS[i,:])
 

parameters.py
@@ -44,12 +44,12 @@ lambd = 1   # influences time kernel value
 sigma2 = 0.01
 
 # PI2 controller
-H = 15  # control horizon steps
-controlCost = 1e1   # affects noise of path roll-outs (negatively)
-R = controlCost*np.eye(H) # input cost matrix
+H = 10  # control horizon steps
+controlCost = 1e0   # affects noise of path roll-outs (negatively)
+R = controlCost*np.eye(H)   # input cost matrix
 g = np.ones((H,1))
-lambd = 1e-1    # rescales state costs, affects noise of path roll-outs (positively)
-K = 30  # number of path roll outs
+lambd = 1e-2    # rescales state costs, affects noise of path roll-outs (positively)
+K = 15  # number of path roll outs
 ctrSamplingTime = 0.01  # time discretization
 nUpdated = 10   # number of iterations
 