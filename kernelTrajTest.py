81d3eeabc496871dd25cdd226bd4ee1e55ff28ab
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-05-23 13:54:51 +0200

    Still no nice results. Continue on path planning with cubis splines now.

--- kernelTrajTest.py
+++ kernelTrajTest.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 
 def generateTrajectory(theta, pos):
     # create anchor points
-    nAnchorPoints = 30
+    nAnchorPoints = 5
     trajectoryNoise = 0.05
+    maxStepsize = 1
 
     lMax = 10
     eps = 1
     delta = 1
-    Traj = np.zeros((2, nAnchorPoints))
 
     anchorPoints = np.zeros((2, nAnchorPoints))
     anchorPoints[:, 0] = pos[:,0]
@@ -20,27 +20,36 @@ def generateTrajectory(theta, pos):
     alpha = math.pi/4
     for i in range(nAnchorPoints - 1):
         alpha = alpha + 2*math.pi * np.random.normal(0,trajectoryNoise)
-        anchorPoints[0, i + 1] = anchorPoints[0, i] + par.maxStepsize / nAnchorPoints * math.cos(alpha)
-        anchorPoints[1, i + 1] = anchorPoints[1, i] + par.maxStepsize / nAnchorPoints * math.sin(alpha)
+        anchorPoints[0, i + 1] = anchorPoints[0, i] + maxStepsize / nAnchorPoints * math.cos(alpha)
+        anchorPoints[1, i + 1] = anchorPoints[1, i] + maxStepsize / nAnchorPoints * math.sin(alpha)
         y[0, i + 1] = y[0, i] + par.dt / nAnchorPoints
         # calculate weight vector
-        GX = np.zeros((nAnchorPoints, nAnchorPoints))
-        GY = np.zeros((nAnchorPoints, nAnchorPoints))
+    GX = np.zeros((nAnchorPoints, nAnchorPoints))
+    GY = np.zeros((nAnchorPoints, nAnchorPoints))
+
+    plt.figure()
+    plt.plot(anchorPoints[0, :], anchorPoints[1, :])
+    plt.title("True field")
+    plt.show()
 
     mPi = np.zeros((nAnchorPoints, nAnchorPoints))
     for i in range(nAnchorPoints):
         for j in range(nAnchorPoints):
             GX[i, j] = RBFkernel(anchorPoints[:, i], anchorPoints[:, j])
             GY[i, j] = RBFkernel(y[0, i], y[0, j])
-            mPi[i, j] = sum(np.random.choice(GX.flatten(), lMax)) / lMax
+
+    GXTriang = np.triu(GX)
+    for i in range(nAnchorPoints):
+        for j in range(nAnchorPoints):
+            mPi[i, j] = sum(np.random.choice(GXTriang[GXTriang != 0], lMax)) / lMax
 
     Traj = np.zeros((2,nAnchorPoints))
     for iy in range(nAnchorPoints):
-        Lambd = np.dot(np.linalg.inv(GX + eps * np.eye(nAnchorPoints)), mPi[:,iy])
+        Lambd = np.dot(np.linalg.inv(GX + nAnchorPoints * eps * np.eye(nAnchorPoints)), mPi[:,iy])
         LambdGY = np.dot(Lambd.T, GY)
-        factor1 = 1/(np.dot(LambdGY, LambdGY.T) + eps)
+        factor1 = 1/(np.dot(LambdGY, LambdGY.T) + delta)
         factor2 = 1/np.dot(LambdGY,Lambd)
-        wy = factor1*factor2*GY[:, iy].reshape(nAnchorPoints,1)
+        wy = factor1*factor2*GY[iy,:].reshape(nAnchorPoints,1)
         Traj[:,iy] = np.dot(anchorPoints, wy)[:,0]
     return Traj
 
