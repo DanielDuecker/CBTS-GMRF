2a6f48224910159afaa34b2112c2f248fab0724f
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-06-01 10:19:44 +0200

    added testfile for debugging

---
+++ mapConDisActionTest.py
@@ -0,0 +1,23 @@
+import parameters as par
+import numpy as np
+from kCBTS import mapActionReward
+
+map = mapActionReward(-1,2,3,3)
+print(map.meanCond)
+print(map.covCond)
+
+print("First update:")
+map.updateMapActionReward(np.array([[0.1],[0.2],[0.3]]),100)
+print(map.meanCond)
+print(map.covCond)
+
+print("Second update:")
+map.updateMapActionReward(np.array([[1],[0.9],[0.8]]),-200)
+
+print("Test on theta=[0.5 0.5 0.5]")
+thetaTest = np.array([[0.5],[0.5],[0.5]])
+print("Cov function:")
+print(map.getCovarianceFromAction(thetaTest))
+
+print("Get best theta:")
+print(map.getBestTheta())
