eb8e3edbfe669d10d0f549342a9e53932920bf65
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-06-17 14:50:30 +0200

    Still working on 2D GP

--- gaussianProcess.py
+++ gaussianProcess.py
@@ -1,11 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class GP:
-    def __init__(self,nTest,kernelPar,bayesianOptPar,varMeas):
-        self.nTest = nTest
+    def __init__(self,kernelPar,varMeas):
         self.kernelPar = kernelPar
-        self.bayesianOptPar = bayesianOptPar
         self.varMeas = varMeas
         self.trainInput = None
         self.trainOutput = None
@@ -13,6 +12,16 @@ class GP:
     def kernel(self,z1,z2):
         squaredDistance = np.linalg.norm(z1-z2,2)
         return np.exp(-.5 * 1/self.kernelPar * squaredDistance)
+
+    def getKernelMatrix(self,vec1,vec2):
+        n = vec1.shape[1]
+        N = vec2.shape[1]
+        K = np.zeros((n,N))
+        for i in range(n):
+            for j in range(N):
+                 K[i,j] = self.kernel(vec1[:,i],vec2[:,j])
+        return K
+        # todo: only update K matrix instead of recalculating
 
     def update(self,input,output):
         if self.trainInput == None:
@@ -23,22 +32,58 @@ class GP:
             self.trainOutput = np.hstack(self.trainOutput,output)
 
     def predict(self,input):
-        K = self.kernel(self.trainInput,self.trainInput)
+        K = self.getKernelMatrix(self.trainInput,self.trainInput)
+        L = np.linalg.cholesky(K + self.varMeas*np.eye(self.trainInput.shape[1]))
 
-        L = np.linalg.cholesky(K + self.varMeas*np.eye(self.trainInput.shape[0]))
-        alpha = np.linalg.solve(L.T,np.linalg.solve(L,self.trainOutput))
+        # Compute mean
+        Lk = np.linalg.solve(L,self.getKernelMatrix(self.trainInput,input))
+        mu = np.dot(Lk.T, np.linalg.solve(L,self.trainOutput))
 
+        print("input:",input)
+        print("mu:",mu)
 
+        # Compute variance
+        KStar = self.getKernelMatrix(input,input)
+        var = KStar - np.dot(Lk.T,Lk)
 
+        return mu, var
 
+# Parameter
+kernelPar = 0.1
+varMeas = 0.01
+kappa = 1
+GP = GP(kernelPar,varMeas)
 
+# Ground Truth
+f = lambda x,y: np.sin(0.9*x)+np.sin(0.9*y)
+xGT0, xGT1 = np.meshgrid(np.linspace(-5,5,1000),np.linspace(-5,5,1000))
+fGT = f(xGT0,xGT1)
 
+# Training data
+nTrain = 10
+xTrain = np.random.uniform(-5,5,(2,nTrain))
+fTrain = f(xTrain[0,:],xTrain[1,:]) + varMeas*np.random.randn(nTrain)
+print("xTrain:",xTrain)
+print("fTrain:",fTrain)
+GP.update(xTrain,fTrain)
 
+nSample = 50
+xSample = np.random.uniform(-5,5,(2,nSample))
+mu,var = GP.predict(xSample)
 
+# acquisition function
+H = mu + kappa*var.diagonal()
+index = np.argmax(H)
 
+fig = plt.figure()
 
+ax1 = fig.add_subplot(221)
+ax1.contourf(xGT0, xGT1, fGT)
+plt.title("True field")
 
+ax2 = fig.add_subplot(222)
+ax2.contourf(xSample[0,:],xSample[1,:],mu)
 
-nTrain = 5
-nTest = 100
+plt.show()
+
 
