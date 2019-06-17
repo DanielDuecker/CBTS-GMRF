bbbdc230daec7936d3a4b8d64ebfbca2aa54cffd
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-06-17 17:09:43 +0200

    Worked on sequential GP update. Not yet working

--- gaussianProcess.py
+++ gaussianProcess.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import  Axes3D
 
 
 class GP:
     def __init__(self,kernelPar,varMeas):
         self.kernelPar = kernelPar
         self.varMeas = varMeas
-        self.trainInput = None
-        self.trainOutput = None
+        self.init = False
 
     def kernel(self,z1,z2):
         squaredDistance = np.linalg.norm(z1-z2,2)
@@ -24,12 +24,13 @@ class GP:
         # todo: only update K matrix instead of recalculating
 
     def update(self,input,output):
-        if self.trainInput == None:
+        if self.init == False:
             self.trainInput = input
             self.trainOutput = output
+            self.init = True
         else:
-            self.trainInput = np.vstack(self.trainInput,input)
-            self.trainOutput = np.vstack(self.trainOutput,output)
+            self.trainInput = np.vstack((self.trainInput,input))
+            self.trainOutput = np.vstack((self.trainOutput,output))
 
     def predict(self,input):
         K = self.getKernelMatrix(self.trainInput,self.trainInput)
@@ -49,43 +50,53 @@ class GP:
         return mu, var
 
 # Parameter
-kernelPar = 10
-varMeas = 0.001
+kernelPar = 0.1
+varMeas = 0.1
 kappa = 1
 GP = GP(kernelPar,varMeas)
 
 # Ground Truth
 f = lambda x,y: x**2 + 0.9*y**2
-xGT0, xGT1 = np.meshgrid(np.linspace(-5,5,1000),np.linspace(-5,5,1000))
+#f = lambda x,y: np.sin(x) + np.sin(y)
+xGT0, xGT1 = np.meshgrid(np.linspace(-5,5,100),np.linspace(-5,5,100))
 fGT = f(xGT0,xGT1)
 #print("fGT:",fGT)
 
-# Training data
-nTrain = 100
-xTrain = np.random.uniform(-5,5,(nTrain,2))
-fTrain = f(xTrain[:,0],xTrain[:,1]) + varMeas*np.random.randn(nTrain)
-GP.update(xTrain,fTrain)
+xTrain = np.random.uniform(-5,5,(1,2))
+xTrainHist = np.zeros((1000,2))
+fTrainHist = np.zeros((1000,1))
 
-nSample = 10
-xSample = np.random.uniform(-5,5,(nSample,2))
-mu,var = GP.predict(xSample)
+for i in range(1000):
+    print(i)
+    # next measurement:
+    fTrain = f(xTrain[:,0],xTrain[:,1]) + varMeas*np.random.randn()
+    GP.update(xTrain,fTrain)
 
-# acquisition function
-H = mu + kappa*var.diagonal()
-index = np.argmax(H)
+    nSample = 20
+    xSample = np.random.uniform(-5,5,(nSample,2))
+    mu,var = GP.predict(xSample)
+    xTrainHist[i,:] = xTrain
+    fTrainHist[i] = fTrain
 
-fig = plt.figure()
+    # acquisition function
+    # todo size of mu changes sometimes. Why?
+    H = mu.reshape(nSample,1) + kappa*var.diagonal().reshape(nSample,1)
+    index = np.argmax(H)
+    xTrain = xSample[index,:].reshape(1,2)
+    #print(H)
+    #print(index)
 
-ax1 = fig.add_subplot(221)
-ax1.contourf(xGT0, xGT1, fGT)
-ax1.plot(xTrain[:,0],xTrain[:,1],"g.")
-ax1.plot(xSample[:,0],xSample[:,1],"r.")
-plt.title("True field")
+    fig = plt.figure()
 
-print("difference:",mu-f(xSample[:,0],xSample[:,1]))
-
-#ax2 = fig.add_subplot(222)
-#ax2.contourf(xSample[0,:],xSample[1,:],mu)
+    ax = fig.add_subplot(111,projection='3d')
+    ax.plot_surface(xGT0, xGT1, fGT)
+    ax.plot(xTrainHist[:,0],xTrainHist[:,1],fTrainHist[:,0],"g.")
+    print(mu.shape)
+    ax.plot(xSample[:,0],xSample[:,1],mu.reshape(20,1),"r.")
+    plt.title("True field")
+    print(xSample[:,0].shape)
+    print(xSample[:,1].shape)
+    print("difference:",mu-f(xSample[:,0],xSample[:,1]))
 
 plt.show()
 
