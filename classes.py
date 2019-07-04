Found major bug. If sequential updating was used for CBTS, the belief update between different nodes was always done with a zero belief and variance (Since bSeq and precCond haven't been updated in STKF, although the gmrf in STKF was passed to the controller

classes.py
@@ -154,7 +154,7 @@ class GP:
 
 
 class gmrf:
-    def __init__(self,par):
+    def __init__(self,par,nGridX,nGridY):
         """GMRF properties"""
         self.par = par
 
@@ -164,8 +164,8 @@ class gmrf:
         self.yMin = par.yMin
         self.yMax = par.yMax
 
-        self.nGridX = par.nGridX
-        self.nGridY = par.nGridY
+        self.nGridX = nGridX
+        self.nGridY = nGridY
 
         self.nEdge = par.nEdge
 
@@ -237,6 +237,11 @@ class gmrf:
         else:
             self.meanCond = np.dot(self.covPrior, np.dot(Phi.T, np.dot(np.linalg.inv(R), zMeas)))
 
+        # Also update bSeq and precCond in case seq. belief update is used for planning
+        self.bSeq = self.bSeq + 1 / self.ov2 * Phi[-1] * zMeas[-1]
+        self.precCond = np.linalg.inv(self.covCond)
+
+
     def seqBayesianUpdate(self, zMeas_k, Phi_k):
         Phi_k = Phi_k.reshape(1, self.nP + self.nBeta)
         zMeas_k = zMeas_k.reshape(1, 1)
@@ -255,6 +260,7 @@ class gmrf:
 
 class stkf:
     def __init__(self,par, gmrf1):
+        self.par = par
         self.gmrf = gmrf1
         self.dt = par.dt
         self.sigmaT = par.sigmaT
@@ -309,10 +315,13 @@ class stkf:
         self.gmrf.covCond = np.dot(self.Cs, np.dot(covt, self.Cs.T))
         self.gmrf.diagCovCond = self.gmrf.covCond.diagonal()
 
+        # Also update bSeq and precCond in case seq. belief update is used for planning
+        self.gmrf.bSeq = self.gmrf.bSeq + 1 / self.par.ov2 * Phi.T * zMeas  # sequential update canonical mean
+        self.gmrf.precCond = np.linalg.inv(self.gmrf.covCond)
 
 class node:
-    def __init__(self, par, gmrf1, auv):
-        self.gmrf = copy.deepcopy(gmrf1)
+    def __init__(self, par, gmrfRed, auv):
+        self.gmrfRed = copy.deepcopy(gmrfRed)
         self.auv = copy.deepcopy(auv)
         self.rewardToNode = 0
         self.accReward = 0

control.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import functions
 from classes import node
+from classes import gmrf
 
 
 class piControl:
@@ -130,9 +131,14 @@ class CBTS:
         self.xTraj = np.zeros((self.nTrajPoints, 1))
         self.yTraj = np.zeros((self.nTrajPoints, 1))
 
-    def getNewTraj(self, auv, gmrf):
+    def getNewTraj(self, auv, gmrfOrig):
+        gmrfRed = gmrfOrig
+        #gmrfRed = gmrf(self.par,self.par.nGridXred,self.par.nGridYred)
+        #functions.sampleGMRF(gmrfOrig,gmrfRed)
+
+        # Get gmrf with less grid points
         print("calculating..")
-        v0 = node(self.par, gmrf, auv)  # create node with belief b and total reward 0
+        v0 = node(self.par, gmrfRed, auv)  # create node with belief b and total reward 0
         self.xTraj = np.zeros((self.nTrajPoints, 1))
         self.yTraj = np.zeros((self.nTrajPoints, 1))
         for i in range(self.nIterations):
@@ -161,7 +167,7 @@ class CBTS:
                 v.GP.update(theta, r)
 
                 # Create new node:
-                vNew = node(self.par, v.gmrf, v.auv)
+                vNew = node(self.par, v.gmrfRed, v.auv)
                 vNew.rewardToNode = v.rewardToNode + self.discountFactor ** v.depth * r
                 vNew.totalReward = vNew.rewardToNode
                 vNew.parent = v
@@ -174,8 +180,8 @@ class CBTS:
                 for i in range(len(o)):
                     vNew.auv.x = traj[0, i + 1]
                     vNew.auv.y = traj[1, i + 1]
-                    Phi = functions.mapConDis(vNew.gmrf, vNew.auv.x, vNew.auv.y)
-                    vNew.gmrf.seqBayesianUpdate(o[i], Phi)
+                    Phi = functions.mapConDis(vNew.gmrfRed, vNew.auv.x, vNew.auv.y)
+                    vNew.gmrfRed.seqBayesianUpdate(o[i], Phi)
 
                 vNew.auv.derivX = derivX
                 vNew.auv.derivY = derivY
@@ -232,7 +238,7 @@ class CBTS:
 
         # plot acquisition function
         if self.par.plotOptions.showAcquisitionFunction:
-            functions.plotRewardFunction(self.par,v0.gmrf)
+            functions.plotRewardFunction(self.par,v0.gmrfRed)
 
         return bestTraj, derivX, derivY
 
@@ -278,11 +284,11 @@ class CBTS:
         r = - self.controlCost * theta
         o = []
         for i in range(self.nTrajPoints - 1):
-            Phi = functions.mapConDis(v.gmrf, tau[0, i + 1], tau[1, i + 1])
-            r += (np.dot(Phi, v.gmrf.covCond.diagonal()) + self.UCBRewardFactor * np.dot(Phi, v.gmrf.meanCond))[0]
-            o.append(np.dot(Phi, v.gmrf.meanCond))
+            Phi = functions.mapConDis(v.gmrfRed, tau[0, i + 1], tau[1, i + 1])
+            r += (np.dot(Phi, v.gmrfRed.covCond.diagonal()) + self.UCBRewardFactor * np.dot(Phi, v.gmrfRed.meanCond))[0]
+            o.append(np.dot(Phi, v.gmrfRed.meanCond))
             # lower reward if agent is out of bound
-            if not functions.sanityCheck(tau[0, i + 1] * np.eye(1), tau[1, i + 1] * np.eye(1), v.gmrf):
+            if not functions.sanityCheck(tau[0, i + 1] * np.eye(1), tau[1, i + 1] * np.eye(1), v.gmrfRed):
                 r -= self.outOfGridPenaltyCBTS
 
         return r, o