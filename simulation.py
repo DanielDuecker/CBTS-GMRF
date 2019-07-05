7863df6f56ffea15c049c81682f0603bd850f51d
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-07-05 09:05:09 +0200

    Implemented switching of simulation options during runtime. Adding legend. Problem: different step sizes of PI2 and CBTS

--- simulation.py
+++ simulation.py
@@ -11,66 +11,26 @@ import shutil
 import pickle
 import copy
 
-"""Simulation Parameters"""
-saveToFile = True
-nSim = 5
-
-"""Get and modify Simulation Parameters"""
+"""Load Parameters"""
 par = copy.deepcopy(parameters.par)
+"""Simulation Options"""
+beliefOptions = ['stkf']  # 'stkf' 'seqBayes', 'regBayes', 'regBayesTrunc'
+controlOptions = ['cbts', 'pi2']  #'cbts', 'pi2', 'randomWalk'
 
-par.nIter = 100
-
-par.belief = 'stkf'  # 'stkf' 'seqBayes', 'regBayes', 'regBayesTrunc'
-par.control = 'pi2'  #'cbts', 'pi2', 'randomWalk'
+saveToFile = True
+nSim = 2
+par.nIter = 20
 par.fieldType = 'predefined'  # 'peak','sine' or 'predefined'
 par.temporal = False  # True: time varying field
 par.plot = False
-parSimList = [par.belief, par.control, par.fieldType, par.temporal]
 
-if par.belief != 'regBayesTrunc':
-    par.nMeas = par.nIter
+parSimList = [nSim, par.nIter, par.belief, par.control, par.fieldType, par.temporal]
+simList = []
+diffMeanDict = {}
+totalVarDict = {}
 
-if par.belief == 'stkf':
-    par.nBeta = 0
-
-"""Simulate different settings:"""
-controlVec = ['cbts','pi2']
-
-"""Initialize"""
-x = []
-y = []
-trueField = []
-gmrf = []
-controller = []
-CBTS = []
-timeVec = []
-xHist = []
-yHist = []
-diffMean = []
-totalVar = []
-parList = []
-
-for control in controlVec:
-    par.control = control
-    for i in range(nSim):
-        print("Simulation ",i," of ",nSim," with ",control)
-        xR, yR, trueFieldR, gmrfR, controllerR, CBTSR, timeVecR, xHistR, yHistR, diffMeanR, totalVarR = main.main(par)
-
-        x.append(xR)
-        y.append(yR)
-        trueField.append(trueFieldR)
-        gmrf.append(gmrfR)
-        controller.append(controllerR)
-        CBTS.append(CBTSR)
-        timeVec.append(timeVecR)
-        xHist.append(xHistR)
-        yHist.append(yHistR)
-        diffMean.append(diffMeanR)
-        totalVar.append(totalVarR)
-        parList.append(par)
-
+"""Create directory if data should be saved"""
 if saveToFile:
-    # Create new directory
     index = 0
     dirpath = os.getcwd()
     folderName = time.strftime("%Y%m%d") + "_" + str(index)
@@ -85,68 +45,118 @@ if saveToFile:
         print("Error while creating directory!")
     os.chdir(path)
 
-    # Copy used parameters
-    shutil.copyfile(dirpath + '/parameters.py',path + '/' + folderName + '_parameters.txt')
+"""Iterate Through Simulation Options"""
+for belief in beliefOptions:
+    par.belief = belief
+    for control in controlOptions:
+        par.control = control
 
-    """Save objects"""
-    # Save objects
-    with open('objs.pkl', 'wb') as f:
-        pickle.dump([x, y, trueField, gmrf, controller, CBTS, timeVec, xHist, yHist, diffMean, totalVar, parList], f)
+        simCase = belief + '_' + control
+        simList.append(simCase)
 
-    # Getting back the objects:
-    #with open('objs.pkl','rb') as f:
-    #    x, y, trueField, gmrf, controller, CBTS, timeVec, xHist, yHist, diffMean, totalVar, parList = pickle.load(f)
+        """Initialize"""
+        x = []
+        y = []
+        trueField = []
+        gmrf = []
+        controller = []
+        CBTS = []
+        timeVec = []
+        xHist = []
+        yHist = []
+        diffMean = []
+        totalVar = []
+        parList = []
 
-    # Save data as csv
-    with open(folderName + '_data.csv','w') as dataFile:
-        writer = csv.writer(dataFile)
+        if par.belief != 'regBayesTrunc':
+            par.nMeas = par.nIter
+        if par.belief == 'stkf':
+            par.nBeta = 0
+
         for i in range(nSim):
-            writer.writerow([i])
-            writer.writerow(x[i])
-            writer.writerow(y[i])
-            writer.writerow(timeVec[i])
-            writer.writerow(xHist[i])
-            writer.writerow(yHist[i])
-            writer.writerow(diffMean[i])
-            writer.writerow(totalVar[i])
-            writer.writerow(gmrf[i].meanCond)
-            writer.writerow(gmrf[i].covCond)
-            writer.writerow(parSimList)
-            writer.writerow(["-"])
-    dataFile.close()
+            print("Simulation ",i," of ",nSim," with ",par.belief, " belief and controller ", par.control)
+            xR, yR, trueFieldR, gmrfR, controllerR, CBTSR, timeVecR, xHistR, yHistR, diffMeanR, totalVarR = main.main(par)
 
-    """Plot data"""
-    fig0 = plt.figure(100, figsize=(19.2,10.8), dpi=100)
-    print("Plotting..")
-    for i in range(nSim):
-        functions.plotFields(par,fig0, x[i], y[i], trueField[i], gmrf[i], controller[i], CBTS[i], timeVec[i], xHist[i], yHist[i])
-        fig0.savefig(folderName + '_' + str(i) + '_' + par.belief + '_' + par.control + '_' + par.fieldType +'.svg', format='svg')
-        plt.clf()
-    plt.close(fig0)
+            x.append(xR)
+            y.append(yR)
+            trueField.append(trueFieldR)
+            gmrf.append(gmrfR)
+            controller.append(controllerR)
+            CBTS.append(CBTSR)
+            timeVec.append(timeVecR)
+            xHist.append(xHistR)
+            yHist.append(yHistR)
+            diffMean.append(diffMeanR)
+            totalVar.append(totalVarR)
+            parList.append(par)
 
-    fig1 = plt.figure(200,figsize=(19.2,10.8), dpi=100)
-    x = np.linspace(0,par.nIter-1,par.nIter-1)
-    plt.title('Performance Measurement')
-    plt.subplot(211)
-    meanDiff = np.mean(diffMean,axis=0)
-    iqrDiff = stats.iqr(diffMean,axis=0)
+        diffMeanDict[simCase] = diffMean
+        totalVarDict[simCase] = totalVar
+
+        # Copy used parameters
+        shutil.copyfile(dirpath + '/parameters.py',path + '/' + folderName + '_' + simCase + '_parameters.txt')
+
+        """Save objects"""
+        # Save objects
+        with open('objs' + '_' + simCase + '.pkl', 'wb') as f:
+            pickle.dump([x, y, trueField, gmrf, controller, CBTS, timeVec, xHist, yHist, diffMean, totalVar, parList], f)
+
+        # Getting back the objects:
+        # with open('objs.pkl','rb') as f:
+        #    x, y, trueField, gmrf, controller, CBTS, timeVec, xHist, yHist, diffMean, totalVar, parList = pickle.load(f)
+
+        # Save data as csv
+        with open(folderName + '_' + simCase + '_data.csv','w') as dataFile:
+            writer = csv.writer(dataFile)
+            for i in range(nSim):
+                writer.writerow([i])
+                writer.writerow(x[i])
+                writer.writerow(y[i])
+                writer.writerow(timeVec[i])
+                writer.writerow(xHist[i])
+                writer.writerow(yHist[i])
+                writer.writerow(diffMean[i])
+                writer.writerow(totalVar[i])
+                writer.writerow(gmrf[i].meanCond)
+                writer.writerow(gmrf[i].covCond)
+                writer.writerow(parSimList)
+                writer.writerow(["-"])
+            dataFile.close()
+
+        """Plot fields"""
+        fig0 = plt.figure(100, figsize=(19.2,10.8), dpi=100)
+        print("Plotting..")
+        for i in range(nSim):
+            functions.plotFields(par,fig0, x[i], y[i], trueField[i], gmrf[i], controller[i], CBTS[i], timeVec[i], xHist[i], yHist[i])
+            fig0.savefig(folderName + '_' + str(i) + '_' + simCase + '_' + par.fieldType +'.svg', format='svg')
+            plt.clf()
+        plt.close(fig0)
+
+fig1 = plt.figure(200,figsize=(19.2,10.8), dpi=100)
+x = np.linspace(0,par.nIter-1,par.nIter-1)
+plt.title('Performance Measurement')
+plt.subplot(211)
+for sim in simList:
+    meanDiff = np.mean(diffMeanDict[sim],axis=0)
+    iqrDiff = stats.iqr(diffMeanDict[sim],axis=0)
     plt.plot(x,meanDiff,'red')
     plt.plot(x,meanDiff - iqrDiff,'gray')
     plt.plot(x,meanDiff + iqrDiff,'gray')
     plt.fill_between(x,meanDiff - iqrDiff,meanDiff + iqrDiff)
-    plt.xlabel('Iteration Index')
-    plt.ylabel('Difference Between Ground Truth and Belief')
-    plt.subplot(212)
-    meanVar = np.mean(totalVar,axis=0)
-    iqrVar = stats.iqr(totalVar,axis=0)
+plt.xlabel('Iteration Index')
+plt.ylabel('Difference Between Ground Truth and Belief')
+plt.subplot(212)
+for sim in simList:
+    meanVar = np.mean(totalVarDict[sim],axis=0)
+    iqrVar = stats.iqr(totalVarDict[sim],axis=0)
     plt.plot(x,meanVar,'red')
     plt.plot(x,meanVar - iqrVar,'gray')
     plt.plot(x,meanVar + iqrVar,'gray')
     plt.fill_between(x,meanVar - iqrVar,meanVar + iqrVar)
-    plt.xlabel('Iteration Index')
-    plt.ylabel('Total Belief Uncertainty')
-    fig1.savefig(folderName + '_performance.svg', format='svg')
-    plt.show()
+plt.xlabel('Iteration Index')
+plt.ylabel('Total Belief Uncertainty')
+fig1.savefig(folderName + '_performance.svg', format='svg')
+plt.show()
 
 # TODO Enable loading of pickled data instead of simulating
 # TODO Enable changing of parameters while simulating
