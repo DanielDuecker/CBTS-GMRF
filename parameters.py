84531e73709ec3c73640bba5ab8d623c81341bef
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-07-12 23:12:27 +0200

    For some reason the spsolve is way slower in my implementation compared to Geist's.Why?

--- parameters.py
+++ parameters.py
@@ -40,10 +40,10 @@ class par:
     """GMRF class"""
     xMin = 0  # GMRF dimensions
     xMax = 10
-    nGridX = 10
+    nGridX = 50
     yMin = 0
     yMax = 10
-    nGridY = 10
+    nGridY = 50
     nBeta = 1  # regression coefficients
     nEdge = 3  # needs to be at least 1
     valueT = 1e-3  # Prior precision value for regression vector bet
