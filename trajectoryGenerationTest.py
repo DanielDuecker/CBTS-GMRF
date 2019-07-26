22fee352716e50e2215d01bf030c5fe056d05c09
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-07-26 12:27:23 +0200

    Implemented deeper trajectories in trajectoryGenerationTest

--- trajectoryGenerationTest.py
+++ trajectoryGenerationTest.py
@@ -10,7 +10,8 @@ import math
 
 matplotlib.use('TkAgg')
 
-N = 500
+N = 10
+depth = 1
 
 par = par('seqBayes', 'cbts', 'noUpdates', 'random', False, False, 1000, 0, 0, 0, 0, 0, 6, 3, 50, 50, 0.05, 0.5, 0.9)
 cbts = CBTS(par)
@@ -18,10 +19,30 @@ auv = agent(par, par.x0, par.y0, par.alpha0)
 gmrf1 = gmrf(par, par.nGridX, par.nGridY, par.nEdge)
 v = node(par, gmrf, auv)
 
+derivXStart = auv.derivX
+derivYStart = auv.derivY
+
 fig = plt.figure()
 for i in range(N):
-    theta = (-1+2*np.random.rand())*np.eye(1)
-    tau, derivX, derivY = cbts.generateTrajectory(v,theta)
+    v.auv.x = par.x0
+    v.auv.y = par.y0
+    v.auv.derivX = derivXStart
+    v.auv.derivY = derivYStart
+    # Get first trajectory
+    tau = np.zeros((2,(par.nTrajPoints-1)*depth+1))
+    theta = (-1 + 2 * np.random.rand()) * np.eye(1)
+    tau[:,0:par.nTrajPoints], v.auv.derivX, v.auv.derivY = cbts.generateTrajectory(v,theta)
+    v.auv.x = tau[0, par.nTrajPoints - 1]
+    v.auv.y = tau[1, par.nTrajPoints - 1]
+
+    # Check next depth
+    for d in range(1,depth):
+        theta = (-1+2*np.random.rand())*np.eye(1)
+        tauNext, v.auv.derivX, v.auv.derivY = cbts.generateTrajectory(v,theta)
+        v.auv.x = tauNext[0,-1]
+        v.auv.y = tauNext[1,-1]
+        tau[:,(par.nTrajPoints-1)*d:(par.nTrajPoints-1)*(d+1)+1] = tauNext
+
     stepSizes = []
     xOld = par.x0
     yOld = par.y0
@@ -29,7 +50,6 @@ for i in range(N):
         stepSizes.append(math.sqrt((xOld-tau[0,i+1])**2+(yOld-tau[1,i+1])**2))
         xOld = tau[0,i+1]
         yOld = tau[1,i+1]
-    test = 1
     plt.plot(tau[0,:],tau[1,:])
     plt.scatter(tau[0,:],tau[1,:])
 print(np.mean(stepSizes))
