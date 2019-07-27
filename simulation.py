8514aa5e1e86844fc3989e13b72e8bf9989dd757
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-07-27 15:28:34 +0200

    Save

--- simulation.py
+++ simulation.py
@@ -15,7 +15,7 @@ matplotlib.use('TkAgg')
 
 """Simulation Options"""
 beliefOptions = ['seqBayes']  # 'stkf' 'seqBayes', 'regBayes', 'regBayesTrunc'
-controlOptions = ['pi2']  # 'cbts', 'pi2', 'randomWalk', 'geist'
+controlOptions = ['pi2','geist']  # 'cbts', 'pi2', 'randomWalk', 'geist'
 cbtsNodeBelief = ['noUpdates']  # 'fullGMRF', 'sampledGMRF', 'noUpdates'
 
 """Simulation Options"""
@@ -25,7 +25,7 @@ nSim = 2
 nIter = 500
 fieldType = 'random'  # 'peak','sine', 'random' or 'predefined'
 temporal = False  # True: time varying field
-plot = True
+plot = False
 "PI2"
 K = [15]
 H = [10]
