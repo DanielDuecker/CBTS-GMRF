Also normalized covariance is now used. Check effect on performance. Should lead to fixed tuning not depending on field values

control.py
@@ -307,7 +307,7 @@ class CBTS:
         o = []
         for i in range(self.nTrajPoints - 1):
             Phi = functions.mapConDis(v.gmrf, tau[0, i + 1], tau[1, i + 1])
-            r += (np.dot(Phi, v.gmrf.diagCovCond) + self.UCBRewardFactor * np.dot(Phi, v.gmrf.meanCond/max(v.gmrf.meanCond)))[0]
+            r += (np.dot(Phi, v.gmrf.diagCovCond / max(v.gmrf.diagCovCond)) + self.UCBRewardFactor * np.dot(Phi, v.gmrf.meanCond/max(v.gmrf.meanCond)))[0]
             o.append(np.dot(Phi, v.gmrf.meanCond))
             # lower reward if agent is out of bound
             if not functions.sanityCheck(tau[0, i + 1] * np.eye(1), tau[1, i + 1] * np.eye(1), v.gmrf):