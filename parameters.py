ac4fd706238a1b058cc4cb76e52d60345c54023c
Benedikt Mersch <benedikt.mersch@gmail.com>
2019-08-08 10:05:24 +0200

    Edited sim par

--- parameters.py
+++ parameters.py
@@ -33,7 +33,7 @@ class par:
         self.x0 = 0
         self.y0 = 0
         self.alpha0 = math.pi / 4  # initial state
-        self.maxStepsize = 0.2  # maximum change in every state per iteration
+        self.maxStepsize = 0.1  # maximum change in every state per iteration
         self.xVel = self.maxStepsize
         self.yVel = self.maxStepsize
 
--- simulation.py
+++ simulation.py
@@ -17,14 +17,14 @@ matplotlib.use('TkAgg')
 
 """Simulation Options"""
 beliefOptions = ['seqBayes']  # 'stkf' 'seqBayes', 'regBayes', 'regBayesTrunc'
-controlOptions = ['pi2','geist','cbts']  # 'cbts', 'pi2', 'randomWalk', 'geist'
+controlOptions = ['cbts']  # 'cbts', 'pi2', 'randomWalk', 'geist'
 cbtsNodeBelief = ['noUpdates']  # 'fullGMRF', 'sampledGMRF', 'noUpdates'
 
 """Simulation Options"""
 printTime = False
 saveToFile = True
 nSim = 5
-nIter = 120
+nIter = 500
 fieldType = 'random'  # 'peak','sine', 'random' or 'predefined'
 temporal = False  # True: time varying field
 plot = True
